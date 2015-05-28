03deb4540d685b1998001b0ce44637ab88032927
Thomas T Gurney <ttgurney@cotse.net>
2015-05-28 13:45:16 -0400

    spdx.py: Remove unused import

--- src/spdx.py
+++ src/spdx.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sqlalchemy.orm.exc import NoResultFound
 import orm
 import os
 import scanners
