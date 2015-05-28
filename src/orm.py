1cd7dd244371f76db2d2c2bf713b981a27162068
Thomas T. Gurney <ttgurney@cotse.net>
2015-05-28 15:53:18 -0500

    Update copyright text

--- src/orm.py
+++ src/orm.py
@@ -1,5 +1,5 @@
 # <SPDX-License-Identifier: Apache-2.0>
-# Copyright (c) 2015 doSOCS contributors.
+# Copyright (c) 2015 University of Nebraska Omaha and other contributors.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
