78fe43b63ef4f1c30d6e7620c0be2fd1584e6df5
Thomas T Gurney <ttgurney@cotse.net>
2015-05-28 16:33:28 -0400

    Update DB init tool with all required static data

--- src/dbinit.py
+++ src/dbinit.py
@@ -1,17 +1,87 @@
 import orm
+import re
+import subprocess
+import sys
+import uuid
 
-session = orm.Session()
-filetypes = (
-    'SOURCE',
-    'BINARY',
-    'ARCHIVE',
-    'APPLICATION',
-    'AUDIO',
-    'IMAGE',
-    'TEXT',
-    'VIDEO',
-    'DOCUMENTATION'
-)
-for f in filetypes:
-    session.add(orm.FileType(name=f))
-session.commit()
+
+def load_file_types(session):
+    filetypes = (
+        'SOURCE',
+        'BINARY',
+        'ARCHIVE',
+        'APPLICATION',
+        'AUDIO',
+        'IMAGE',
+        'TEXT',
+        'VIDEO',
+        'DOCUMENTATION'
+        )
+    for f in filetypes:
+        session.add(orm.FileType(name=f))
+
+
+def load_licenses(session):
+    rows = scrape_site()
+    sorted_rows = list(sorted(rows))
+    for shortname, name, url in sorted_rows:
+        license_params = {
+            'name': name,
+            'short_name': shortname,
+            'cross_reference': url,
+            'comment': '',
+            'is_spdx_official': True,
+            'license_identifier': 'LicenseRef-' + str(uuid.uuid4())
+            }
+        session.add(orm.License(**license_params))
+
+
+def load_creator_types(session):
+    creator_types = (
+        'PERSON',
+        'ORGANIZATION',
+        'TOOL'
+        )
+    for c in creator_types:
+        session.add(orm.CreatorType(name=c))
+
+
+def load_annotation_types(session):
+    annotation_types = (
+        'REVIEW',
+        'OTHER'
+        )
+    for a in annotation_types:
+        session.add(orm.AnnotationType(name=a))
+
+
+def scrape(page_text):
+    '''Scrape license info and return (url, name, shortname) tuples'''
+    url_part = r'<tr>\s*<td><a href=\"(.*?)\".*?>'
+    name_part = r'(.*?)</a></td>\s*.*?'
+    shortname_part = r'<code property=\"spdx:licenseId\">(.*?)</code>'
+    pattern_str = url_part + name_part + shortname_part
+    pattern = re.compile(pattern_str)
+    page_one_line = page_text.replace('\n', '')
+    rows = pattern.findall(page_one_line)
+    return rows
+
+
+def scrape_site(url='http://spdx.org/licenses/'):
+    page_text = subprocess.check_output(['curl', url])
+    rows = scrape(page_text)
+    # reverse column order and append full url to first column
+    completed_rows = [[row[2], row[1], (url + row[0][2:])]
+                      for row in rows
+                      ]
+    return completed_rows
+
+
+def main():
+    session = orm.Session()
+    load_licenses(session)
+    load_creator_types(session)
+    load_annotation_types(session)
+    load_file_types(session)
+    session.commit()
+    session.close()
