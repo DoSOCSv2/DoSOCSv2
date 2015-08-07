3491e445c2667cd3726efd1efb1f41e114433926
Thomas T Gurney <ttgurney@cotse.net>
2015-08-07 09:36:48 -0400

    Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dosocs2',
-    version='0.12.2',
+    version='0.12.3',
     description='SPDX 2.0 document creation and storage',
     long_description='',
     url='https://github.com/ttgurney/dosocs2',
