d29e53de5f932fa8ff73963afa77417223d1bffa
Thomas T. Gurney <ttgurney@cotse.net>
2015-10-13 10:38:00 -0500

    Update install-nomos.sh

--- scripts/install-nomos.sh
+++ scripts/install-nomos.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+NOMOSSA_PATH=/usr/local/bin/nomossa
+if [[ -f $NOMOSSA_PATH ]]; then
+    echo "[$0] Looks like nomos is already installed at $NOMOSSA_PATH"
+    echo "[$0] Doing nothing."
+    exit 0
+fi
+
 if [[ "$(type -t git)" == "" ]]; then
-    echo "Couldn't find git in your PATH!"
-    echo "Please install git before continuing."
+    echo "[$0] Couldn't find git in your PATH!"
+    echo "[$0] Please install git before continuing."
     exit 1
 fi
 
 if [[ ! -d /usr/include/glib-2.0 ]]; then
-    echo "Couldn't find glib-2.0 headers!"
-    echo "Please install glib2-devel before continuing."
+    echo "[$0] Couldn't find glib-2.0 headers!"
+    echo "[$0] Please install glib2-devel before continuing."
     exit 1
 fi
 
 if [[ "$(type -t gcc)" == "" ]]; then
-    echo "Couldn't find gcc in your PATH!"
-    echo "Please install gcc before continuing."
+    echo "[$0] Couldn't find gcc in your PATH!"
+    echo "[$0] Please install gcc before continuing."
     exit 1
 fi
 
@@ -30,6 +37,6 @@ sudo make install || exit 1
 popd
 echo "[$0] rm -rf $REPO"
 rm -rf $REPO
-echo "[$0] sudo ln -s /usr/local/share/fossology/nomos/agent/nomossa /usr/local/bin/nomossa"
-sudo ln -s /usr/local/share/fossology/nomos/agent/nomossa /usr/local/bin/nomossa || exit 1
+echo "[$0] sudo ln -s /usr/local/share/fossology/nomos/agent/nomossa $NOMOSSA_PATH"
+sudo ln -s /usr/local/share/fossology/nomos/agent/nomossa $NOMOSSA_PATH || exit 1
 echo "[$0] done!"
