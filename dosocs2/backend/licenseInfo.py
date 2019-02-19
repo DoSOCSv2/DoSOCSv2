d7190d8865696face70d04b2260f4a6df543e2cb
Nebrethar <msnell@unomaha.edu>
2019-02-19 12:59:21 -0600

    Add quick frontend and API services

--- dosocs2/backend/licenseInfo.py
+++ dosocs2/backend/licenseInfo.py
@@ -16,35 +16,31 @@ def retrieve_license_information(owner, repo):
 
     temp = open("temp.txt", "r+")
     subprocess.call(['sudo', 'git', 'clone', repo_url, cwd + '/repodl/' + repo], shell=False)
-    pope = subprocess.Popen(['sudo', 'dosocs2', 'oneshot', cwd + '/repodl/' + repo, '-T', cwd + '/2.0.tag.3'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pope = subprocess.Popen(['sudo', 'dosocs2', 'oneshot', cwd + '/repodl/' + repo], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     subprocess.call(['sudo', 'dosocs2', 'oneshot', cwd + '/repodl/' + repo], shell=False, stdout=temp)
     out, err = pope.communicate()
-    #if out:
-        #print(out)
+    if out:
+        print(out)
     if err:
          print(err.decode('UTF-8'))
-    results = re.findall(r'(PackageName): (.*)\n(SPDXID): (.*)\n(PackageVersion|):? ?(.*|)\n?(PackageFileName): (.*)\n(PackageSupplier): (.*)\n(PackageOriginator): (.*)\n(PackageDownloadLocation): (.*)\n(PackageVerificationCode): (.*)\n(PackageChecksum|):? ?(.*|)\n?(PackageHomePage): (.*)\n(PackageLicenseConcluded): (.*)\n*(PackageLicenseDeclared): (.*)\n(PackageLicenseComments): (.*)\n(PackageCopyrightText): (.*)\n(PackageSummary): (.*)\n(PackageDescription): (.*)\n(PackageComment): (.*|)', out.decode('UTF-8'))
-    results_l = re.findall(r'(PackageLicenseInfoFromFiles): (.*)\n?', out.decode('UTF-8')),
 
-    #print(results_l)
-    license_information = []
-    #print('\n')
-    #print(results_l)
-    #print('\n')
+    splitOut = out.decode('UTF-8').splitlines()
+    #print(splitOut)
     temp = {}
-    for i in range(0, 17):
-        j = i*2
-        temp[results[0][j]] = results[0][j+1]
-
-    license_information.append(temp)
-
-    temp_l = {}
-    i = 0
-    for i in range(0, len(results_l[0])):
-        temp_l["License #" + str(i)] = results_l[0][i][1]
-        i += 1
-    #print(temp_l)
-    license_information.append(temp_l)
-    #dfinal = dict(temp)
-    #dfinal.update(temp_l)
-    return json.dumps(license_information)
+    license_info = []
+    j = 0
+    section = "NONE"
+    for value in splitOut:
+        if ":" in value:
+            valSplit = value.split(":")
+            print(valSplit)
+            temp[str(j) + " " + valSplit[0]] = valSplit[1]
+            j += 1
+        if "#" in value:
+            if bool(temp) != 0:
+                license_info.append(temp)
+            section = value
+            j = 0
+            temp = {}
+    print(temp)
+    return json.dumps(license_info)
