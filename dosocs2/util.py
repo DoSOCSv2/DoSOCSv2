1d017f30729c709fb512e5816871c4a547f8fa8b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 19:26:53 +0000

    Adding tarfile member sanitization to extractall()

--- dosocs2/util.py
+++ dosocs2/util.py
@@ -99,7 +99,26 @@ def tempextract(path):
         if ar_type == 'tar':
             with tarfile.open(path) as tf:
                 relpaths = tf.getnames()
-                tf.extractall(path=tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tf, path=tempdir)
             yield (tempdir, relpaths)
         elif ar_type == 'zip':
             with zipfile.ZipFile(path) as zf:
